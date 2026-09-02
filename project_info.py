d5cbef5a247dcbd3802bcec932e38bff91e18012
zhangyhrs <cumtzyh@gmail.com>
2026-09-02 07:32:27 -0700

    Add Python project metadata file

---
+++ project_info.py
@@ -0,0 +1,13 @@
+"""
+Map Tile Downloader (MTDL)
+
+Programming language: Python
+GUI framework: PyQt5
+Application type: Windows desktop GIS / Remote Sensing tool
+
+This repository distributes the compiled application and documentation.
+The complete application source code is not publicly released.
+"""
+
+PROJECT_NAME = "Map Tile Downloader"
+LANGUAGE = "Python"
